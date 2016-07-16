c370f1e2d9e258d145e59a853d9536532d798029
Don Nguyen <dnguyen793@gmail.com>
2016-07-16 14:00:08 -0700

    Added empy .ino to fill when ready

---
+++ ENPH253/ENPH253.ino
@@ -0,0 +1,15 @@
+#include <phys253.h>
+#include <LiquidCrystal.h>
+
+#include "Graph.c"
+
+void setup() {
+  #include <phys253setup.txt>
+  Serial.begin(9600);
+  LCD.clear();
+}
+
+void loop() {
+    
+}
+
